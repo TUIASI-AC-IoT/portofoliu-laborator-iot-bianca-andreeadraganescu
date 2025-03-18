493790416c3b03b1f813b4a378fdd9aaf2306a0d
bianca-andreeadraganescu <100412791+bianca-andreeadraganescu@users.noreply.github.com>
2025-03-18 17:42:21 +0200

    Laborator5

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* WiFi station Example
+/*  WiFi softAP Example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -9,321 +9,175 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/event_groups.h"
 #include "esp_system.h"
-#include "driver/gpio.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-#include "esp_http_client.h"
-#include "esp_https_ota.h"
-#include "esp_tls.h"
+#include "freertos/event_groups.h"
+#include "esp_http_server.h"
+
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
-#include "lwip/sockets.h"
-#include "lwip/netdb.h"
+#include "soft-ap.h"
+#include "http-server.h"
 
-#include "version.h"
+#include "../mdns/include/mdns.h"
+
 
 #define CONFIG_ESP_WIFI_SSID      "lab-iot"
 #define CONFIG_ESP_WIFI_PASS      "IoT-IoT-IoT"
 #define CONFIG_ESP_MAXIMUM_RETRY  5
 #define CONFIG_LOCAL_PORT         10001
-
-//TODO: Modificati adresa IP de mai jos pentru a coincide cu cea a PC-ul pe care rulati scriptul python
 #define CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL "https://192.168.89.43:5000/firmware.bin" 
 
-#define GPIO_OUTPUT_IO 4
-#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_OUTPUT_IO)
-#define GPIO_INPUT_IO 2
-#define GPIO_INPUT_PIN_SEL (1ULL<<GPIO_INPUT_IO)
-#define MAX_HTTP_OUTPUT_BUFFER 2048
-#define MAX_HTTP_RECV_BUFFER 512
+static const char *TAG = "wifi softAP";
 
-/* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t s_wifi_event_group;
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+static const char *TAG_SCAN = "scan";
+static const char *TAG_SOFTAP = "wifi softAP";
 
-static EventGroupHandle_t s_event_start_ota;
-#define BIT_BTN_PRESSED    BIT0
+static char *SSIDs = "";
 
-static const char *TAG = "simple_ota_example";
-extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
-extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
-
-static int s_retry_num = 0;
-
-esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+static void print_auth_mode(int authmode)
 {
-    static char *output_buffer;  // Buffer to store response of http request from event handler
-    static int output_len;       // Stores number of bytes read
-    switch (evt->event_id) {
-    case HTTP_EVENT_ERROR:
-        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+    switch (authmode) {
+    case WIFI_AUTH_OPEN:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_OPEN");
         break;
-    case HTTP_EVENT_ON_CONNECTED:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+    case WIFI_AUTH_WEP:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WEP");
         break;
-    case HTTP_EVENT_HEADER_SENT:
-        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+    case WIFI_AUTH_WPA_PSK:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WPA_PSK");
         break;
-    case HTTP_EVENT_ON_HEADER:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+    case WIFI_AUTH_WPA2_PSK:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WPA2_PSK");
         break;
-    case HTTP_EVENT_ON_DATA:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
-        // Clean the buffer in case of a new request
-        if (output_len == 0 && evt->user_data) {
-            // we are just starting to copy the output data into the use
-            memset(evt->user_data, 0, MAX_HTTP_OUTPUT_BUFFER);
-        }
-        /*
-        *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
-        *  However, event handler can also be used in case chunked encoding is used.
-        */
-        if (!esp_http_client_is_chunked_response(evt->client)) {
-            // If user_data buffer is configured, copy the response into the buffer
-            int copy_len = 0;
-            if (evt->user_data) {
-                // The last byte in evt->user_data is kept for the NULL character in case of out-of-bound access.
-                copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
-                if (copy_len) {
-                    memcpy(evt->user_data + output_len, evt->data, copy_len);
-                }
-            } else {
-                int content_len = esp_http_client_get_content_length(evt->client);
-                if (output_buffer == NULL) {
-                    // We initialize output_buffer with 0 because it is used by strlen() and similar functions therefore should be null terminated.
-                    output_buffer = (char *) calloc(content_len + 1, sizeof(char));
-                    output_len = 0;
-                    if (output_buffer == NULL) {
-                        ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
-                        return ESP_FAIL;
-                    }
-                }
-                copy_len = MIN(evt->data_len, (content_len - output_len));
-                if (copy_len) {
-                    memcpy(output_buffer + output_len, evt->data, copy_len);
-                }
-            }
-            output_len += copy_len;
-        }
-
+    case WIFI_AUTH_WPA_WPA2_PSK:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WPA_WPA2_PSK");
         break;
-    case HTTP_EVENT_ON_FINISH:
-        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WPA2_ENTERPRISE");
         break;
-    case HTTP_EVENT_DISCONNECTED:
-        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+    case WIFI_AUTH_WPA3_PSK:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WPA3_PSK");
         break;
-    case HTTP_EVENT_REDIRECT:
-        ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+    case WIFI_AUTH_WPA2_WPA3_PSK:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_WPA2_WPA3_PSK");
         break;
-    }
-    return ESP_OK;
-}
-
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    default:
+        ESP_LOGI(TAG_SCAN, "Authmode \tWIFI_AUTH_UNKNOWN");
+        break;
     }
 }
 
-bool wifi_init_sta(void)
+static void print_cipher_type(int pairwise_cipher, int group_cipher)
 {
-    s_wifi_event_group = xEventGroupCreate();
+    switch (pairwise_cipher) {
+    case WIFI_CIPHER_TYPE_NONE:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_NONE");
+        break;
+    case WIFI_CIPHER_TYPE_WEP40:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_WEP40");
+        break;
+    case WIFI_CIPHER_TYPE_WEP104:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_WEP104");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_TKIP");
+        break;
+    case WIFI_CIPHER_TYPE_CCMP:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_CCMP");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP_CCMP:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_TKIP_CCMP");
+        break;
+    default:
+        ESP_LOGI(TAG_SCAN, "Pairwise Cipher \tWIFI_CIPHER_TYPE_UNKNOWN");
+        break;
+    }
 
+    switch (group_cipher) {
+    case WIFI_CIPHER_TYPE_NONE:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_NONE");
+        break;
+    case WIFI_CIPHER_TYPE_WEP40:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_WEP40");
+        break;
+    case WIFI_CIPHER_TYPE_WEP104:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_WEP104");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_TKIP");
+        break;
+    case WIFI_CIPHER_TYPE_CCMP:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_CCMP");
+        break;
+    case WIFI_CIPHER_TYPE_TKIP_CCMP:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_TKIP_CCMP");
+        break;
+    default:
+        ESP_LOGI(TAG_SCAN, "Group Cipher \tWIFI_CIPHER_TYPE_UNKNOWN");
+        break;
+    }
+}
+
+/* Initialize Wi-Fi as sta and set scan method */
+static void wifi_scan(void)
+{
     ESP_ERROR_CHECK(esp_netif_init());
-
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
+    uint16_t number = 5;
+    wifi_ap_record_t ap_info[5];
+    uint16_t ap_count = 0;
+    memset(ap_info, 0, sizeof(ap_info));
 
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASS,
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
-                CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASS);
-        return true;
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASS);
-    } else {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
-    }
-    return false;
-}
-
-static void ota_task(void *pvParameters)
-{
-    xEventGroupWaitBits(s_event_start_ota, BIT_BTN_PRESSED, pdTRUE, pdTRUE, portMAX_DELAY);
-
-    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER + 1] = {0};
-    ESP_LOGI(TAG, "Starting OTA example task");
-    esp_http_client_config_t config = {
-        .url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL,
-        .cert_pem = (char *)server_cert_pem_start,
-        .cert_len = 1422,
-        .event_handler = _http_event_handler,
-        .keep_alive_enable = true,
-        .use_global_ca_store = true,
-        .user_data = local_response_buffer,
-        .skip_cert_common_name_check = true
-    };
-
-    esp_https_ota_config_t ota_config = {
-        .http_config = &config,
-    };
-    
-    ESP_ERROR_CHECK(esp_tls_init_global_ca_store());
-    ESP_ERROR_CHECK(esp_tls_set_global_ca_store((unsigned char*)server_cert_pem_start, server_cert_pem_end - server_cert_pem_start));
-
-    ESP_LOGI(TAG, "Attempting to download update from %s", config.url);
-    esp_err_t ret = esp_https_ota(&ota_config);
-    if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "OTA Succeed, Rebooting...");
-        esp_restart();
-    } else {
-        ESP_LOGE(TAG, "Firmware upgrade failed");
-    }
-
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-
-    esp_err_t err = esp_http_client_perform(client);
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %"PRId64,
-                esp_http_client_get_status_code(client),
-                esp_http_client_get_content_length(client));
-    } else {
-        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
-    }
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-static void button_task(void * pvParameter)
-{
-    uint8_t u8Count = 5;
-    int val = 1;
-
-    while(1)
-    {
-        if (gpio_get_level(GPIO_INPUT_IO) != val)
-            u8Count--;
-        else
-            u8Count = 5;
-
-        if(!u8Count) {
-            val = gpio_get_level(GPIO_INPUT_IO);
-            
-            if(!gpio_get_level(GPIO_INPUT_IO)){
-                ESP_LOGI(TAG, "Button pressed");
-                xEventGroupSetBits(s_event_start_ota, BIT_BTN_PRESSED);
-                val = gpio_get_level(GPIO_INPUT_IO);
-            }
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    esp_wifi_scan_start(NULL, true);
+    ESP_LOGI(TAG_SCAN, "Max AP number ap_info can hold = %u", number);
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    ESP_LOGI(TAG_SCAN, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
+    for (int i = 0; i < number; i++) {
+        ESP_LOGI(TAG_SCAN, "SSID \t\t%s", ap_info[i].ssid);
+        ESP_LOGI(TAG_SCAN, "RSSI \t\t%d", ap_info[i].rssi);
+        print_auth_mode(ap_info[i].authmode);
+        if (ap_info[i].authmode != WIFI_AUTH_WEP) {
+            print_cipher_type(ap_info[i].pairwise_cipher, ap_info[i].group_cipher);
         }
-
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG_SCAN, "Channel \t\t%d\n", ap_info[i].primary);
     }
+    printf("%s", SSIDs);
+
 }
 
-void gpio_init()
-{
-    //zero-initialize the config structure.
-    gpio_config_t io_conf = {};
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
-}
 
 void app_main(void)
 {
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
-    gpio_init();
+    // TODO: 3. Pornire mod STA + scanare SSID-uri disponibile
+    wifi_scan();
+    // TODO: 4. Initializare mDNS (daca mai ramana timp)    
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    bool connected = wifi_init_sta();
+    // TODO: 1. Pornire softAP
 
-    if (connected) {
-        s_event_start_ota = xEventGroupCreate();
-        xTaskCreate(ota_task, "ota_task", 8192, NULL, 5, NULL);
-        xTaskCreate(button_task, "button_task", 4096, NULL, 5, NULL);
-    }
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+    // TODO: 2. Pornire server web (si config specifice in http-server.c) 
 }
